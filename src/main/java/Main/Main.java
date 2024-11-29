2c4ba8dfb3628f959129718f1f59b4b0ef2b99f1
karoll1naa <karollinnajan@gmail.com>
2024-11-29 20:06:08 +0200

    Updating the Main

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -2,6 +2,9 @@ package Main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String inputText = "I still haven't done the lab on     operating systems(1). I don't like this subject(2). Can I remove it from the program?(3)";
+        Text text = new Text(inputText);
+        TextProcessor processor = new TextProcessor();
+        processor.sortSentencesByWordCount(text);
     }
 }
